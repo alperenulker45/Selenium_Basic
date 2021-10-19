fc0d5a147e10e6b3b6c77010635dcf7703c4ee34
alperenulker45” <ulkeralperen@gmail.com>
2021-10-19 13:43:14 +0300

    Sample6Task Done

--- src/selenium/sample/Sample6Task.java
+++ src/selenium/sample/Sample6Task.java
@@ -3,8 +3,10 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
@@ -34,21 +36,78 @@ public class Sample6Task {
     public void findElementByXPath() throws Exception {
 //         TODO:
 //        2 ways to find text: "Heading 2 text":
+        WebElement heading2Text = driver.findElement(By.xpath("//h2[@id='heading_2']"));
+        driver.findElement(By.xpath("//h2[text()='Heading 2 text']"));//second way
+
+        assertEquals("Heading 2 text", heading2Text.getText());
+
 //        1-2 ways to find text: "Test Text 1"
+        WebElement testTxt1 = driver.findElement(By.xpath("//div[@id='test1']//p[@class='test']"));
+        driver.findElement(By.xpath("//p[text()='Test Text 1']"));//second way
+
+        assertEquals("Test Text 1", testTxt1.getText());
+
 //        1-2 ways to find text: "Test Text 2"
+        WebElement testTxt2 = driver.findElement(By.xpath("//div[@id='test1']//p[@class='twoTest']"));
+        driver.findElement(By.xpath("//p[text()='Test Text 2']"));//second way
+
+        assertEquals("Test Text 2", testTxt2.getText());
+
 //        1-2 ways to find text: "Test Text 3"
+        WebElement testTxt3 = driver.findElement(By.xpath("//div[@id='test3']//p[@class='test']"));
+        driver.findElement(By.xpath("//p[text()='Test Text 3']"));//second way
+
+        assertEquals("Test Text 3", testTxt3.getText());
+
+//
 //        1-2 ways to find text: "Test Text 4"
+        WebElement testTxt4 = driver.findElement(By.xpath("//div[@id='test3']//p[@class='test'][2]"));
+        driver.findElement(By.xpath("//p[text()='Test Text 4']"));//second way
+
+        assertEquals("Test Text 4", testTxt4.getText());
+
 //        1-2 ways to find text: "Test Text 5"
+       WebElement testTxt5 = driver.findElement(By.xpath("//div[@id='test2']//p[@class='Test']"));
+        driver.findElement(By.xpath("//p[text()='Test Text 5']"));//second way
+
+        assertEquals("Test Text 5", testTxt5.getText());
+
 //        1-2 ways to find text: "This is also a button"
+        WebElement randomBtn2 = driver.findElement(By.xpath("//input[@name='randomButton2']"));
+        driver.findElement((By.xpath("//input[@type='button'][@id='buttonId']")));//second way
+
+        assertEquals("This is also a button", randomBtn2.getAttribute("value"));
     }
 
     @Test
     public void findElementByCssName() throws Exception {
 //         TODO:
 //        1-2 ways to find text: "Heading 2 text"
+        WebElement heading2Txt = driver.findElement(By.cssSelector("#heading_2"));
+        driver.findElement(By.cssSelector("h2[id='heading_2']"));//second way
+
+        assertEquals("Heading 2 text", heading2Txt.getText());
+
 //        1-2 ways to find text: "Test Text 1"
+        WebElement testTxt1 = driver.findElement(By.cssSelector("#test1>p"));
+        driver.findElement(By.cssSelector("#test1>p.test"));//second way
+
+        assertEquals("Test Text 1", testTxt1.getText());
+
 //        1-2 ways to find text: "Test Text 2"
+        WebElement testTxt2 = driver.findElement(By.cssSelector("#test1>p.twoTest"));
+        assertEquals("Test Text 2", testTxt2.getText());
+
 //        1-2 ways to find text: "Test Text 3"
+        WebElement testTxt3 = driver.findElement(By.cssSelector("div#test3 > p:nth-child(1)"));
+        driver.findElement(By.cssSelector("div#test3 > p.test"));//second way
+
+        assertEquals("Test Text 3", testTxt3.getText());
+
 //        1-2 ways to find text: "This is also a button"
+        WebElement randomBtn2 = driver.findElement(By.cssSelector("input[name='randomButton2']"));
+        driver.findElement(By.cssSelector("input#buttonID"));//second way
+
+        assertEquals("This is also a button", randomBtn2.getAttribute("value"));
     }
 }
