aa7ab24cc08fba7cf7680e26ff6ac3cba4afe99e
alperenulker45” <ulkeralperen@gmail.com>
2021-10-19 14:52:02 +0300

    Sample7Task Done

--- src/selenium/sample/Sample7Task.java
+++ src/selenium/sample/Sample7Task.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.util.List;
@@ -45,6 +46,32 @@ public class Sample7Task {
 //        tick  "Option 3"
 //        click result
 //        check that text 'You selected value(s): Option 2, Option 3' is being displayed
+
+        List<WebElement> checkBoxList = driver.findElements(By.cssSelector(".w3-check[type='checkbox']"));
+        try {
+            for (WebElement e : checkBoxList) {
+                assertFalse(e.isSelected());
+            }
+        }catch (AssertionError e) {
+            System.out.println("Checkbox should not be selected");
+            e.printStackTrace();
+        }
+
+        checkBoxList.get(1).click(); //click option 2
+        assertFalse(checkBoxList.get(0).isSelected());
+        assertFalse(checkBoxList.get(2).isSelected());
+
+        WebElement option3 = driver.findElement(By.cssSelector(".w3-check[value='Option 3'][type='checkbox']"));
+        option3.click();
+
+        WebElement resultChckBoxBtn = driver.findElement(By.id("result_button_checkbox"));
+        resultChckBoxBtn.click();
+
+        WebElement displayedTxt = driver.findElement(By.xpath("//p[contains(text(), 'selected')]"));
+        String expectedTxt = "You selected value(s): Option 2, Option 3";
+
+        assertEquals(expectedTxt, displayedTxt.getText());
+
     }
 
 
@@ -58,6 +85,36 @@ public class Sample7Task {
 //        check that "Option 2" and "Option 3' are not select, but "Option 1" is selected
 //        click result
 //        check that 'You selected option: Option 1' text is being displayed
+
+        List<WebElement> radioBtns = driver.findElements(By.cssSelector(".w3-check[type='radio']"));
+        try {
+            for (WebElement e : radioBtns) {
+                assertFalse(e.isSelected());
+            }
+        }catch (AssertionError e) {
+            System.out.println("Radio button should not be selected");
+            e.printStackTrace();
+        }
+
+        WebElement option3 = driver.findElement(By.cssSelector("[value='Option 3'][type='radio']"));
+        option3.click();
+
+        assertFalse(radioBtns.get(0).isSelected()); // checking is option 1 selected
+        assertFalse(radioBtns.get(1).isSelected()); // checking is option 2 selected
+
+        radioBtns.get(0).click(); //option 1 clicked
+
+        assertFalse(radioBtns.get(1).isSelected()); // checking is option 2 selected
+        assertFalse(option3.isSelected()); // checking is option 3 selected
+
+        WebElement radioResultBtn = driver.findElement(By.id("result_button_ratio"));
+        radioResultBtn.click();
+
+        WebElement radioDisplayedTxt = driver.findElement(By.id("result_radio"));
+        String expectedTxt = "You selected option: Option 1";
+
+        assertEquals(expectedTxt, radioDisplayedTxt.getText());
+
     }
 
     @Test
@@ -68,6 +125,22 @@ public class Sample7Task {
 //        check that selected option is "Option 2"
 //        click result
 //        check that 'You selected option: Option 2' text is being displayed
+
+        Select dropdown = new Select(driver.findElement(By.id("vfb-12")));
+        dropdown.selectByVisibleText("Option 3");
+        assertEquals("Option 3", dropdown.getFirstSelectedOption().getText());
+
+        dropdown.selectByIndex(2);
+        assertEquals("Option 2", dropdown.getFirstSelectedOption().getText());
+
+        WebElement resultBtn = driver.findElement(By.cssSelector("#result_button_select"));
+        resultBtn.click();
+
+        WebElement resultMsg = driver.findElement(By.cssSelector("#result_select"));
+        String expectedMsg = "You selected option: Option 2";
+
+        assertEquals(expectedMsg, resultMsg.getText());
+
     }
 
     @Test
@@ -75,6 +148,34 @@ public class Sample7Task {
 //         TODO:
 //        enter date '4 of July 2007' using calendar widget
 //        check that correct date is added
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        dateBox.clear();
+        assertEquals("", dateBox.getAttribute("value"));
+
+        dateBox.click();
+
+        WebElement dateWidget = driver.findElement(By.id("ui-datepicker-div"));
+//    go back 10 month in calendar on page
+        for (int i = 0; i < 171; i++) {
+            dateWidget.findElement(By.className("ui-datepicker-prev")).click();
+        }
+
+        dateWidget.findElement(By.xpath("//a[text()='4']")).click();
+        driver.findElement(By.id("result_button_date")).click(); //click on result button
+        Thread.sleep(2000);
+
+        WebElement displayedTxt = driver.findElement(By.id("result_date"));
+
+        String expectedTxt = "You entered date: 07/04/2007";
+
+        try {
+            assertEquals(expectedTxt, displayedTxt.getText());
+        }catch (AssertionError e) {
+            System.out.println("Result should be : \"You entered date: 07/04/2007\"");
+            e.printStackTrace();
+        }
+
+
     }
 
     @Test
@@ -82,5 +183,12 @@ public class Sample7Task {
 //         TODO:
 //        enter date '2 of May 1959' using text
 //        check that correct date is added
+
+        WebElement dateBox = driver.findElement(By.id("vfb-8"));
+        dateBox.clear();
+        dateBox.sendKeys("05/02/1959");
+        Thread.sleep(2000);
+
+        assertEquals("05/02/1959", dateBox.getAttribute("value"));
     }
 }
